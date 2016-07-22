828e0fc79077fc8de3920f9bf1f8f8be0daf1763
Yassine Boukhata <yacine_r@yahoo.fr>
2016-07-22 14:30:56 +0200

    import wakanda client

--- src/vendor.ts
+++ src/vendor.ts
@@ -9,5 +9,8 @@ import '@angular/router';
 // RxJS
 import 'rxjs';
 
+// import wakanda client
+import 'wakanda-client';
+
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
